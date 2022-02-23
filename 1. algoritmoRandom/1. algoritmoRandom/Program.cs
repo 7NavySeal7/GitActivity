8d246e7c5d6a6ce9d057f161014dc743c7f91d36
NavySeal <jsmontenegro009@misena.edu.co>
2022-02-23 15:20:02 -0500

    Iniciando proyecto algoritmo random

---
+++ 1. algoritmoRandom/1. algoritmoRandom/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1._algoritmoRandom
+{
+    class Program
+    {
+        static void NumeroAleatorio()
+        {
+            Random generador = new Random();
+            int num = generador.Next(1, 100);
+            Console.WriteLine($"El número generado es: {num}");
+
+
+        }
+        
+        static void Main(string[] args)
+        {
+            NumeroAleatorio();
+        }
+    }
+}
